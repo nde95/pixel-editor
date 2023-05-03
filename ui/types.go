698e42c59eef7b90b13174345cb302f620f1021c
nde95 <ndennison268@gmail.com>
2023-05-03 16:52:36 -0400

    rendering canvas with color picker

--- ui/types.go
+++ ui/types.go
@@ -2,12 +2,14 @@ package ui
 
 import (
 	"pixel/apptype"
+	"pixel/pxcanvas"
 	"pixel/swatch"
 
 	"fyne.io/fyne/v2"
 )
 
 type AppInit struct {
+	PixelCanvas *pxcanvas.PxCanvas
 	PixelWindow fyne.Window
 	State       *apptype.State
 	Swatches    []*swatch.Swatch
